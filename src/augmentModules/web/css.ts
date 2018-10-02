d16ef38adaa8744897bbecb4a0fbfb5012a68fa9
arogg <arogg@users.noreply.github.com>
2018-10-02 10:09:11 +0200

    Refactored css module

--- src/augmentModules/web/css.ts
+++ src/augmentModules/web/css.ts
@@ -1,5 +1,5 @@
 import { ArrayLiteralExpression, SourceFile, FunctionExpression, VariableStatementStructure, VariableDeclarationKind } from "ts-simple-ast";
-import { addToArray, ternary, L, and, write } from "../../utils";
+import { addToArray, ternary, L, and, write, Literal } from "../../utils";
 import * as Generator from 'yeoman-generator';
 import * as path from 'path';
 import { AugmentModule, Extension } from "../../common/augmentModule";
@@ -12,7 +12,7 @@ declare global {
     }
     interface Config {
         cssModules?: boolean;
-        tsdecl?: boolean;
+        createTsCssDeclarationFile?: boolean;
     }
 }
 
@@ -30,11 +30,11 @@ export default class implements Extension {
                 default: true,
                 message: 'Use CSS modules?'
             }),
-            confirmPrompt<'tsdecl'>({
+            confirmPrompt<'createTsCssDeclarationFile'>({
                 when: (resp) => {
                     return resp.ts && resp.cssModules;
                 },
-                name: 'tsdecl',
+                name: 'createTsCssDeclarationFile',
                 default: true,
                 message: `Create TypeScript declaration file for *.css, *.scss and *.sass imports? This is required for working css modules. You will have to make sure your tsconfig.json includes this file in its configuration. It will be named \'${CSS_DECL_FILENAME}\'.`
             }),
@@ -46,24 +46,46 @@ export default class implements Extension {
     AugmentModule = class extends AugmentModule {
 
         augment() {
-            const { type, cssModules, ts: ts2, tsdecl } = this.config;
+            const { type, cssModules, ts, createTsCssDeclarationFile } = this.config;
 
             if (type !== 'web') return;
 
-            const { extractcss, cssfilename } = this.options;
+            const { extractcss } = this.options;
 
-            const ts = !!cssModules && !!ts2;
+            let styleLoaderExpr: any;
+            if (typeof extractcss === 'undefined') {
+                this.addExtractPluginImport();
 
-            const useCssExtractPlugin = 'useCssExtractPlugin';
+                const extractCssVarName = 'useCssExtractPlugin';
+                this.insertVariableStatement([{
+                    name: extractCssVarName,
+                    initializer: `!${this.isDev}`
+                }]);
 
-            const useTsModuleLoader = ts && cssModules;
-            this.insertVariableStatement([{
-                name: useCssExtractPlugin,
-                initializer: typeof extractcss === 'boolean' ? extractcss.toString() : `!${this.isDev}`
-            }]);
+                this.addPlugins(and(extractCssVarName, this.getNewExtractPluginText()));
 
-            if (useTsModuleLoader && tsdecl) {
+                styleLoaderExpr = ternary(extractCssVarName, L('MiniCssExtractPlugin.loader'), 'style-loader');
+
+            } else if (extractcss) {
+                this.addExtractPluginImport();
+
+                this.addPlugins(this.getNewExtractPluginText());
+
+                styleLoaderExpr = L('MiniCssExtractPlugin.loader');
+            } else {
+                styleLoaderExpr = 'style-loader';
+            }
+
+            if (createTsCssDeclarationFile) {
                 this.gen.fs.write(this.gen.destinationPath(CSS_DECL_FILENAME), this.gen.fs.read(this.gen.templatePath(CSS_DECL_FILENAME)));
+            }
+
+            const tsCssModules = ts && cssModules;
+
+            if (tsCssModules) {
+                this.addPlugins(
+                    write(L('new webpack.WatchIgnorePlugin('), [/\.(sa|sc|c)ss\.d\.ts$/i], L(')'))
+                )
             }
 
             const cssLoaderModuleOptions = {
@@ -76,8 +98,8 @@ export default class implements Extension {
                 {
                     test: /\.(sa|sc|c)ss$/i,
                     use: [
-                        ternary(useCssExtractPlugin, L('MiniCssExtractPlugin.loader'), 'style-loader'),
-                        useTsModuleLoader
+                        styleLoaderExpr,
+                        tsCssModules
                             ? {
                                 loader: "typings-for-css-modules-loader",
                                 options: {
@@ -85,10 +107,12 @@ export default class implements Extension {
                                     ...cssLoaderModuleOptions
                                 }
                             }
-                            : cssModules ? {
-                                loader: 'css-loader',
-                                options: cssLoaderModuleOptions
-                            } : 'css-loader',
+                            : cssModules
+                                ? {
+                                    loader: 'css-loader',
+                                    options: cssLoaderModuleOptions
+                                }
+                                : 'css-loader',
                         {
                             loader: 'postcss-loader',
                             options: {
@@ -102,27 +126,22 @@ export default class implements Extension {
                         'sass-loader'
                     ]
                 })
+        }
 
+        private addExtractPluginImport() {
             this.addImportDeclaration({
                 namespaceImport: 'MiniCssExtractPlugin',
                 moduleSpecifier: 'mini-css-extract-plugin'
             })
-            this.addPlugins(
-                and(useCssExtractPlugin, write(L('new MiniCssExtractPlugin('), {
-                    filename: cssfilename || ternary(this.isDev, '[name].css', '[name].[hash].css'),
-                    chunkFilename: ternary(this.isDev, '[id].css', '[id].[hash].css')
-                }, L(')')))
-            )
+        }
 
-            if (useTsModuleLoader) {
-                this.addImportDeclaration({
-                    namespaceImport: 'webpack',
-                    moduleSpecifier: 'webpack'
-                })
-                this.addPlugins(
-                    write(L('new webpack.WatchIgnorePlugin('), [/\.(sa|sc|c)ss\.d\.ts$/i], L(')'))
-                )
-            }
+        private getNewExtractPluginText() {
+            const { cssfilename } = this.options;
+
+            return write(L('new MiniCssExtractPlugin('), {
+                filename: cssfilename || ternary(this.isDev, '[name].css', '[name].[hash].css'),
+                chunkFilename: ternary(this.isDev, '[id].css', '[id].[hash].css')
+            }, L(')'))
         }
     }
 }
--- webpack-template/webpack.config.ts
+++ webpack-template/webpack.config.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as webpack from 'webpack';
 
 /*return*/
 export default (
